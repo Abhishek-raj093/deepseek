cd14c4d1e85b4d852962ebe63029d12c226a6abf
Abhishek-raj093 <ss0986825@gmail.com>
2025-03-20 05:06:41 +0530

    Frontend Complete

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,16 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { useClerk, UserButton } from '@clerk/nextjs'
+import { useAppContext } from '@/confid/context/AppContext'
+import ChatLabel from './ChatLabel'
 
 const Sidebar = ({expand, setExpand}) => {
+
+  const {openSignIn} = useClerk()
+  const {user} = useAppContext()
+  const [openMenu, setOpenMenu] = useState({id:0, open: false})
+
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
       <div>
@@ -33,7 +41,7 @@ const Sidebar = ({expand, setExpand}) => {
 
         <div className={`mt-8 text-white/25 text-sm ${expand ? "block" : "hidden"}`}>
           <p className='my-1'>Recents</p>
-          {/* chatLabel */}
+          <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
         </div>
       </div>
 
@@ -50,8 +58,14 @@ const Sidebar = ({expand, setExpand}) => {
           {expand && <> <span>Get App</span> <Image alt='' src={assets.new_icon}/></>}
         </div>
         
-        <div>
-
+        <div onClick={user ? null : openSignIn}
+         className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 tesxt-sm p-2 mst-2 cursor-pointer`}>
+          {
+            user ? <UserButton/>
+            : <Image src={assets.profile_icon} alt='' className='w-7'/>
+          }
+          
+          {expand && <span>My Profile</span>}
         </div>
         
       </div>
