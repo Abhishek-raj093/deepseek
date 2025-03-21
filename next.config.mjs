be862efa42a6db235851b1d5410200dd783a4d84
Abhishek-raj093 <ss0986825@gmail.com>
2025-03-21 15:43:27 +0530

    env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-    env: {
-        MONGODB_URI: process.env.MONGODB_URI,
-        SIGNING_SECRET: process.env.SIGNING_SECRET,
-    },
-};
+
 export default nextConfig;
 // 
