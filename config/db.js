726932571c152b8275c6d68656470e915c1cd1a0
Abhishek-raj093 <ss0986825@gmail.com>
2025-03-21 16:00:12 +0530

    env

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ let cached = global.mongoose || {conn: null, promise: null};
 export default async function connectDB(){
     if(cached.conn) return cached.conn;
     if(!cached.promise){
-        cached.promise = mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI).then((mongoose) => mongoose);
+        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
     }
     try {
         cached.conn = await cached.promise;
